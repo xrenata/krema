b9902210bbd0ecda88c09f1d94ca77f0428942d3
5elenay <5elenay@protonmail.com>
2021-07-27 20:09:53 +0300

    feat(gateway): added resume handler.

--- src/gateway.py
+++ src/gateway.py
@@ -45,19 +45,46 @@ class Gateway:
         self._zlib = decompressobj()
         self._session: aiohttp.ClientSession = None
 
+        self._seq: int = None
+        self._session_id: str = None
+
         self._event_loop = asyncio.get_event_loop()
+
+        self.client.events.append(
+            ("ready", self.__handle_session_id)
+        )
 
     async def __connect(self):
         """Start WebSocket connection."""
 
+        self.websocket, self._session = None, None
+
         self._session = aiohttp.ClientSession()
         self.websocket = await self._session.ws_connect(self.gateway)
+
+        # Send Resume
+        if self._session_id is not None:
+            print("resume at kardes")
+            await self.websocket.send_json({
+                "op": 6,
+                "d": {
+                    "token": self.token,
+                    "session_id": self._session_id,
+                    "seq": self._seq
+                }
+            })
+
+    async def __handle_session_id(self, packet):
+        self._session_id = packet.get("session_id")
 
     async def __receiver(self):
         """Receive messages from WebSocket."""
 
         while True:
-            await self.__handle_packet(await self.websocket.receive())
+            result = await self.__handle_packet(await self.websocket.receive())
+
+            if result is not None:
+                return result
 
     async def __handle_packet(self, packet):
         """Handle Packets."""
@@ -67,7 +94,7 @@ class Gateway:
         if isinstance(packet.data, int) and len(str(packet.data)) == 4:
             # Reconnect Websocket
             if packet.data == 1001:
-                self._session.close()
+                await self._session.close()
                 self._session = None
                 return 1
             else:
@@ -76,11 +103,12 @@ class Gateway:
                 return 0
         elif isinstance(packet.data, type(None)):
             if packet.type == 0x101:
-                return 0x0
+                return 0
 
         if len(data) < 4 or data[-4:] != b'\x00\x00\xff\xff':
             return
 
+        # Compressor Decode
         self._buffer.extend(data)
         message = self._zlib.decompress(self._buffer).decode("utf-8")
         message = loads(message)
@@ -89,6 +117,12 @@ class Gateway:
 
         opcode, data, seq, event_type = message.get(
             "op"), message.get("d"), message.get("s"), message.get("t")
+
+        if seq is not None:
+            self._seq = seq
+
+        print(self._seq, self._session_id)
+        print(message, end="\n\n")
 
         if opcode == self.HELLO:
             interval = data["heartbeat_interval"]
@@ -100,7 +134,9 @@ class Gateway:
                 self._event_loop.create_task(
                     self.__handle_event(data, event_type))
 
-            # print(message, end="\n\n")
+        # Reconnect
+        elif opcode == self.RECONNECT:
+            return 1
 
     async def __handle_event(self, event_data, event_type):
         if event_type in ("message_create", "message_update"):
@@ -152,13 +188,12 @@ class Gateway:
         """Send hearbeat to the WebSocket."""
 
         while True:
+            await asyncio.sleep(interval // 1000)
             if self.websocket is not None:
                 await self.websocket.send_json({
                     "op": self.HEARTBEAT,
-                    "d": None
+                    "d": self._seq
                 })
-
-                await asyncio.sleep(interval // 1000)
 
     async def start_connection(self):
         """Start the Gateway Connection."""
@@ -166,6 +201,7 @@ class Gateway:
         await self.__connect()
         result = await self.__receiver()
 
-        # Reconnect Websocket
+        # Reconnect
         if result == 1:
-            self.start_connection()
+            print("reconnect kardes.")
+            await self.start_connection()
